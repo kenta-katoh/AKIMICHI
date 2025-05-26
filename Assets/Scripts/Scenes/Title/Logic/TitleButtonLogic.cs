facf4d6e54132b45824b2835581cec39a7b8d6f9
alim\takuya.hozumi <takuya.hozumi@a-lim.co.jp>
2025-05-27 02:11:07 +0900

    タイトルボタン仮追加

---
+++ Assets/Scripts/Scenes/Title/Logic/TitleButtonLogic.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Akimichi.Game;
+using Manager;
+using Akimichi.Data;
+
+namespace Akimichi.Title
+{
+    public class TitleButtonLogic : LogicBase
+    {
+        public TitleButtonLogic(ViewBase view) : base(view)
+        {
+        }
+
+        public void ChangeScene()
+        {
+            // テストでリザルトへ
+            SceneManager.Instance().ChangeScene("ResultScene");
+        }
+
+        public void CreateData()
+        {
+            // テストデータ作成
+            var data = new TestDataBase();
+            data.test = 15;
+            data.SetValue(DataConst.TestStringValueKey,"111");
+            DataManager.Instance().SetData(DataConst.TestDataBaseKey,data);
+        }
+    }
+}
+
